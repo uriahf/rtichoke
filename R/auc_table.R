de596ac4ff56d17948bd8a133532b7ca0ac93d3b
Uriah Finkel <ufinkel@gmail.com>
2022-08-15 15:49:08 +0300

    Support filtering for more than 5 populations when calling `create_table_for_auc()`

--- R/auc_table.R
+++ R/auc_table.R
@@ -38,7 +38,18 @@
 #' 
 #'
 #' @keywords internal
-create_table_for_auc <- function(probs, reals) {
+create_table_for_auc <- function(probs, 
+                                 reals,
+                                 col_values = c("#1b9e77", "#d95f02", 
+                                                "#7570b3", "#e7298a", 
+                                                "#07004D", "#E6AB02", 
+                                                "#FE5F55", "#54494B", 
+                                                "#006E90" , "#BC96E6",
+                                                "#52050A", "#1F271B", 
+                                                "#BE7C4D", "#63768D", 
+                                                "#08A045", "#320A28", 
+                                                "#82FF9E", "#2176FF", 
+                                                "#D1603D", "#585123")) {
   
   if (length(probs) == 1) {
     names(probs) <- "Model 1"
@@ -93,11 +104,26 @@ create_table_for_auc <- function(probs, reals) {
               key_num <- as.character(key_num)
 
               color <- switch(as.character(key_num),
-                "1" = "#5BC0BE",
-                "2" = "#FC8D62",
-                "3" = "#8DA0CB",
-                "4" = "#E78AC3",
-                "5" = "#A4243B"
+                              "1" = col_values[1],
+                              "2" = col_values[2],
+                              "3" = col_values[3],
+                              "4" = col_values[4],
+                              "5" = col_values[5],
+                              "6" = col_values[6],
+                              "7" = col_values[7],
+                              "8" = col_values[8],
+                              "9" = col_values[9],
+                              "10" = col_values[10],
+                              "11" = col_values[11],
+                              "12" = col_values[12],
+                              "13" = col_values[13],
+                              "14" = col_values[14],
+                              "15" = col_values[15],
+                              "16" = col_values[16],
+                              "17" = col_values[17],
+                              "18" = col_values[18],
+                              "19" = col_values[19],
+                              "20" = col_values[20]
               )
 
               badge <- status_badge(color = color)
