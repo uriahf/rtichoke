5d15efded6fa5403d30698508fa9d70e99e41e3c
Uriah Finkel <Ufinkel@gmail.com>
2026-08-20 15:40:30 +0300

    Tighten decision type validation tests

--- tests/testthat/test-decision.R
+++ tests/testthat/test-decision.R
@@ -1,14 +1,16 @@
-test_that("type assert works", {
+test_that("decision type is validated", {
   expect_error(
     create_decision_curve(
       probs = list(example_dat$estimated_probabilities),
       reals = list(example_dat$outcome),
       type = "decision"
-    )
+    ),
+    "should be one of"
   )
 
   expect_error(
     one_pop_one_model %>%
-      plot_decision_curve(type = "decision")
+      plot_decision_curve(type = "decision"),
+    "should be one of"
   )
 })
