85cb74aa1f672c1f7b224e30138cf5413d97e6ba
Uriah Finkel <Ufinkel@gmail.com>
2026-08-20 21:36:28 +0300

    Migrate data-raw population pipeline to native pipe

--- data-raw/multiple_populations.R
+++ data-raw/multiple_populations.R
@@ -2,16 +2,16 @@
 
 multiple_populations <- prepare_performance_data(
   probs = list(
-    "train" = example_dat %>%
-      dplyr::filter(type_of_set == "train") %>%
+    "train" = example_dat |>
+      dplyr::filter(type_of_set == "train") |>
       dplyr::pull(estimated_probabilities),
-    "test" = example_dat %>% dplyr::filter(type_of_set == "test") %>%
+    "test" = example_dat |> dplyr::filter(type_of_set == "test") |>
       dplyr::pull(estimated_probabilities)
   ),
   reals = list(
-    "train" = example_dat %>% dplyr::filter(type_of_set == "train") %>%
+    "train" = example_dat |> dplyr::filter(type_of_set == "train") |>
       dplyr::pull(outcome),
-    "test" = example_dat %>% dplyr::filter(type_of_set == "test") %>%
+    "test" = example_dat |> dplyr::filter(type_of_set == "test") |>
       dplyr::pull(outcome)
   )
 )
