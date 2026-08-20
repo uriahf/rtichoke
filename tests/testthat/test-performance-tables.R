626bd4ef5ec4834755297a62dd98884e1b048a73
Uriah Finkel <Ufinkel@gmail.com>
2026-08-20 21:36:03 +0300

    Migrate performance-table tests to native pipe

--- tests/testthat/test-performance-tables.R
+++ tests/testthat/test-performance-tables.R
@@ -29,16 +29,16 @@ test_that("Performance Table names are correct", {
   expect_equal(
     names(prepare_performance_data(
       probs = list(
-        "train" = example_dat %>%
-          dplyr::filter(type_of_set == "train") %>%
+        "train" = example_dat |>
+          dplyr::filter(type_of_set == "train") |>
           dplyr::pull(estimated_probabilities),
-        "test" = example_dat %>% dplyr::filter(type_of_set == "test") %>%
+        "test" = example_dat |> dplyr::filter(type_of_set == "test") |>
           dplyr::pull(estimated_probabilities)
       ),
       reals = list(
-        "train" = example_dat %>% dplyr::filter(type_of_set == "train") %>%
+        "train" = example_dat |> dplyr::filter(type_of_set == "train") |>
           dplyr::pull(outcome),
-        "test" = example_dat %>% dplyr::filter(type_of_set == "test") %>%
+        "test" = example_dat |> dplyr::filter(type_of_set == "test") |>
           dplyr::pull(outcome)
       )
     )),
