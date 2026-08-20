1b185fcd52ebbd5548eebf24ed3b9a1a2d16df2c
Uriah Finkel <Ufinkel@gmail.com>
2026-08-21 02:55:38 +0300

    Remove commented-out test code

--- tests/testthat/test-prepare_performance_data.R
+++ tests/testthat/test-prepare_performance_data.R
@@ -4,14 +4,6 @@ test_that("Estimated Probabilities are in Range", {
     reals = c(example_dat$outcome, 1)
   ))
 
-  # expect_error(prepare_performance_data(
-  #   probs = list(
-  #     "First Model" = c(example_dat$estimated_probabilities, 1.1),
-  #     "Second Model" = c(example_dat$random_guess, -0.2)
-  #   ),
-  #   reals = c(example_dat$outcome, 1)
-  # ))
-
   expect_error(
     prepare_performance_data(
       probs = list(
@@ -42,11 +34,6 @@ test_that("Estimated Probabilities are in Range", {
 
 
 test_that("Outcomes are 1 or 0", {
-  # expect_error(prepare_performance_data(
-  #   probs = c(example_dat$estimated_probabilities, 0.2),
-  #   reals = c(example_dat$outcome, 1.2)
-  # ))
-
   expect_error(prepare_performance_data(
     probs = list(
       "First Model" = c(example_dat$estimated_probabilities, 1.1),
@@ -54,22 +41,4 @@ test_that("Outcomes are 1 or 0", {
     ),
     reals = c(example_dat$outcome, 2)
   ))
-
-  # expect_error(
-  #   prepare_performance_data(
-  #     probs = list(
-  #       "train" = example_dat |>
-  #         dplyr::filter(type_of_set == "train") |>
-  #         dplyr::pull(estimated_probabilities),
-  #       "test" = c(example_dat |> dplyr::filter(type_of_set == "test") |>
-  #         dplyr::pull(estimated_probabilities), 0.2)
-  #     ),
-  #     reals = list(
-  #       "train" = example_dat |> dplyr::filter(type_of_set == "train") |>
-  #         dplyr::pull(outcome),
-  #       "test" = c(example_dat |> dplyr::filter(type_of_set == "test") |>
-  #         dplyr::pull(outcome), 2)
-  #     )
-  #   )
-  # )
 })
