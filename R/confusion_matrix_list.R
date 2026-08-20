6842e8158c2bbea991627c47a9b8f7621b3367ba
Uriah Finkel <Ufinkel@gmail.com>
2026-08-20 21:40:33 +0300

    Migrate confusion-matrix pipelines to native pipe

--- R/confusion_matrix_list.R
+++ R/confusion_matrix_list.R
@@ -7,42 +7,42 @@
 #' @examples
 #' \dontrun{
 #'
-#' one_pop_one_model %>%
+#' one_pop_one_model |>
 #'   create_conf_mat_list()
 #'
-#' one_pop_one_model_by_ppcr %>%
+#' one_pop_one_model_by_ppcr |>
 #'   create_conf_mat_list()
 #'
-#' multiple_models %>%
+#' multiple_models |>
 #'   create_conf_mat_list()
 #'
-#' multiple_models_by_ppcr %>%
+#' multiple_models_by_ppcr |>
 #'   create_conf_mat_list()
 #'
-#' multiple_populations %>%
+#' multiple_populations |>
 #'   create_conf_mat_list()
 #'
-#' multiple_populations_by_ppcr %>%
+#' multiple_populations_by_ppcr |>
 #'   create_conf_mat_list()
 #' }
 create_conf_mat_list <- function(performance_table,
                                  stratified_by = "probability_threshold") {
   if (stratified_by != "probability_threshold") {
-    performance_table <- performance_table %>%
+    performance_table <- performance_table |>
       dplyr::arrange(.data$ppcr)
   } else {
-    performance_table <- performance_table %>%
+    performance_table <- performance_table |>
       dplyr::arrange(.data$probability_threshold)
   }
 
-  matrix_list <- performance_table %>%
-    dplyr::mutate(Predicted_Positive = .data$predicted_positives) %>%
+  matrix_list <- performance_table |>
+    dplyr::mutate(Predicted_Positive = .data$predicted_positives) |>
     dplyr::mutate(
       Predicted_Negative = .data$FN + .data$TN,
       Real_Positive = .data$TP + .data$FN,
       Real_Negative = .data$TN +.data$ FP,
       N = .data$TP + .data$FP + .data$FN + .data$TN
-    ) %>%
+    ) |>
     dplyr::select(
       .data$probability_threshold,
       .data$ppcr,
@@ -55,21 +55,21 @@ create_conf_mat_list <- function(performance_table,
       .data$Real_Positive,
       .data$Real_Negative,
       .data$N
-    ) %>%
-    mutate(idx = 1:n()) %>%
-    split(f = .["idx"]) %>%
-    purrr::map(~ dplyr::select(., -.data$probability_threshold, -.data$ppcr, -.data$idx)) %>%
-    purrr::map(~ matrix(., nrow = 3, byrow = TRUE)) %>%
+    ) |>
+    mutate(idx = 1:n()) |>
+    (\(data) split(data, f = data["idx"]))() |>
+    purrr::map(~ dplyr::select(., -.data$probability_threshold, -.data$ppcr, -.data$idx)) |>
+    purrr::map(~ matrix(., nrow = 3, byrow = TRUE)) |>
     purrr::map(~ magrittr::set_rownames(., c(
       "Predicted Positive",
       "Predicted Negative", " "
-    ))) %>%
+    ))) |>
     purrr::map(~ magrittr::set_colnames(., c(
       "Real Positive",
       "Real Negative", " "
     )))
 
-  matrix_list %>%
+  matrix_list |>
     purrr::map(~ render_reactable_confusion_matrix(.))
 }
 
