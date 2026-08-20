1cd03cbd564270a3655109c5ab86591849465613
Uriah Finkel <Ufinkel@gmail.com>
2026-08-20 21:36:17 +0300

    Migrate performance-data tests to native pipe

--- tests/testthat/test-prepare_performance_data.R
+++ tests/testthat/test-prepare_performance_data.R
@@ -15,16 +15,16 @@ test_that("Estimated Probabilities are in Range", {
   expect_error(
     prepare_performance_data(
       probs = list(
-        "train" = example_dat %>%
-          dplyr::filter(type_of_set == "train") %>%
+        "train" = example_dat |>
+          dplyr::filter(type_of_set == "train") |>
           dplyr::pull(estimated_probabilities),
-        "test" = c(example_dat %>% dplyr::filter(type_of_set == "test") %>%
+        "test" = c(example_dat |> dplyr::filter(type_of_set == "test") |>
           dplyr::pull(estimated_probabilities), -0.2)
       ),
       reals = list(
-        "train" = example_dat %>% dplyr::filter(type_of_set == "train") %>%
+        "train" = example_dat |> dplyr::filter(type_of_set == "train") |>
           dplyr::pull(outcome),
-        "test" = c(example_dat %>% dplyr::filter(type_of_set == "test") %>%
+        "test" = c(example_dat |> dplyr::filter(type_of_set == "test") |>
           dplyr::pull(outcome), 1)
       )
     )
@@ -49,16 +49,16 @@ test_that("Outcomes are 1 or 0", {
   # expect_error(
   #   prepare_performance_data(
   #     probs = list(
-  #       "train" = example_dat %>%
-  #         dplyr::filter(type_of_set == "train") %>%
+  #       "train" = example_dat |>
+  #         dplyr::filter(type_of_set == "train") |>
   #         dplyr::pull(estimated_probabilities),
-  #       "test" = c(example_dat %>% dplyr::filter(type_of_set == "test") %>%
+  #       "test" = c(example_dat |> dplyr::filter(type_of_set == "test") |>
   #         dplyr::pull(estimated_probabilities), 0.2)
   #     ),
   #     reals = list(
-  #       "train" = example_dat %>% dplyr::filter(type_of_set == "train") %>%
+  #       "train" = example_dat |> dplyr::filter(type_of_set == "train") |>
   #         dplyr::pull(outcome),
-  #       "test" = c(example_dat %>% dplyr::filter(type_of_set == "test") %>%
+  #       "test" = c(example_dat |> dplyr::filter(type_of_set == "test") |>
   #         dplyr::pull(outcome), 2)
   #     )
   #   )
