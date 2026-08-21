e968d8d98c24e33f76a174c68f31be7e20595982
Uriah Finkel <Ufinkel@gmail.com>
2026-08-21 04:28:41 +0300

    Add calibration browser proof script

---
+++ scripts/rtichoke_viz_calibration_proof.R
@@ -0,0 +1,14 @@
+library(rtichoke)
+
+calibration_curve_list <- create_calibration_curve_list(
+  probs = list(
+    "Estimated model" = example_dat$estimated_probabilities,
+    "Random guess" = example_dat$random_guess
+  ),
+  reals = list(example_dat$outcome)
+)
+
+rtichoke:::write_rtichoke_viz_calibration_proof(
+  calibration_curve_list,
+  file.path("docs", "rtichoke-viz-calibration")
+)
